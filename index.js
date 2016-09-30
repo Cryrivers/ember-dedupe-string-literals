8782197f2daad2d6ecdfc969a8839c956d54f6cf
Wang Zhongliang <wangz@garena.com>
2016-09-30 11:07:37 +0800

    only dedupe string literals for production build

--- index.js
+++ index.js
@@ -7,17 +7,16 @@ module.exports = {
   name: 'ember-string-obfuscator',
   included: function(app) {
     this._super.included.apply(this, arguments);
-
-    var dedupeStringLiterals = BabelDedupeStringLiterals();
-
-    dedupeStringLiterals.baseDir = function() {
-      return __dirname;
-    };
-
-    if (app.options.babel.plugins) {
-      app.options.babel.plugins.push(dedupeStringLiterals);
-    } else {
-      app.options.babel.plugins = [dedupeStringLiterals];
+    if (app.env === 'production') {
+      var dedupeStringLiterals = BabelDedupeStringLiterals();
+      dedupeStringLiterals.baseDir = function() {
+        return __dirname;
+      };
+      if (app.options.babel.plugins) {
+        app.options.babel.plugins.push(dedupeStringLiterals);
+      } else {
+        app.options.babel.plugins = [dedupeStringLiterals];
+      }
     }
   }
 };
