d511d0f5072cd4b4cd346256420811b97410254b
Wang Zhongliang <wangz@garena.com>
2016-10-07 13:23:30 +0800

    Prepare for releasing 1.0.0-beta.1

--- index.js
+++ index.js
@@ -4,11 +4,21 @@
 var BabelDedupeStringLiterals = require('./lib/BabelDedupeStringLiterals');
 
 module.exports = {
-  name: 'ember-string-obfuscator',
+  name: 'ember-dedupe-string-literals',
+  setupPreprocessorRegistry: function(type, registry) {
+    if (registry.app.options) {
+      this.dedupeStringLiterals = registry.app.options.dedupeStringLiterals || {};
+      this.dedupeStringLiterals.enabled = this.dedupeStringLiterals.enabled || false;
+      this.dedupeStringLiterals.minimumOccurence = this.dedupeStringLiterals.minimumOccurence || 2;
+      this.dedupeStringLiterals.minimumStringLength = this.dedupeStringLiterals.minimumStringLength || 0;
+      this.dedupeStringLiterals.dedupeObjectKeyString = this.dedupeStringLiterals.dedupeObjectKeyString || false;
+      this.dedupeStringLiterals.dedupeObjectKeyIdentifier = this.dedupeStringLiterals.dedupeObjectKeyIdentifier || false;
+    }
+  },
   included: function(app) {
     this._super.included.apply(this, arguments);
-    if (app.env === 'production') {
-      var dedupeStringLiterals = BabelDedupeStringLiterals();
+    if (this.dedupeStringLiterals.enabled) {
+      var dedupeStringLiterals = BabelDedupeStringLiterals(this.dedupeStringLiterals);
       dedupeStringLiterals.baseDir = function() {
         return __dirname;
       };
